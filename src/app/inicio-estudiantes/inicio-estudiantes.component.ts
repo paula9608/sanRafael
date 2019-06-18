a898e4f3cb28a2c0d29f6395d1bf6770421cc461
paula <parojasr@uqvirtual.edu.co>
2019-06-18 00:51:08 -0500

    inicioEstudiante

--- src/app/inicio-estudiantes/inicio-estudiantes.component.ts
+++ src/app/inicio-estudiantes/inicio-estudiantes.component.ts
@@ -9,10 +9,20 @@ import { IUsuario } from '../interfaces/usuario.interface';
 export class InicioEstudiantesComponent implements OnInit {
 
   nombre: string;
+  grado: string;
   constructor() { }
 
   ngOnInit() {
     this.nombre =  localStorage.getItem('nombre');
+    this.grado= localStorage.getItem('grado');
+  }
+  reproducir(ruta: string, repetir: boolean) {
+    const audio = new Audio(ruta);
+    if (repetir == true) {
+      audio.loop = true;
+    }
+    audio.play();
+
   }
 
 }
